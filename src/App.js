8a68d1616e02c7425e393fe747e6c47526d44a4d
fraberto001@gmail.com <fraberto001@gmail.com>
2022-11-24 17:29:02 +0100

    commit finale

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const [data , setData]=useState([])
 
-  useEffect(()=>{fetch('https://jsonplaceholder.typicode.com/'+content)
+  useEffect(()=>{fetch('https://jsonplaceholder.typicode.com/'+ content)
   .then((response) => response.json())
   .then((json) => setData(json))}, [content])
   
